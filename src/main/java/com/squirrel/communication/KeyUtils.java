14294ecacd8cbefcc26609d20b4d5d1813347d86
anil <anil>
2021-02-17 19:21:03 +1100

    Communication-service - PDF signing with dummy key (2/2)

--- src/main/java/com/squirrel/communication/KeyUtils.java
+++ src/main/java/com/squirrel/communication/KeyUtils.java
@@ -1,5 +1,10 @@
 package com.squirrel.communication;
 
+import com.itextpdf.text.Document;
+import com.itextpdf.text.Image;
+import com.itextpdf.text.Rectangle;
+import com.itextpdf.text.pdf.*;
+import com.itextpdf.text.pdf.security.*;
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 import org.bouncycastle.jce.provider.BouncyCastleProvider;
@@ -8,10 +13,7 @@ import org.bouncycastle.util.io.pem.PemReader;
 import org.springframework.util.ResourceUtils;
 
 import java.io.*;
-import java.security.KeyFactory;
-import java.security.NoSuchAlgorithmException;
-import java.security.PrivateKey;
-import java.security.Security;
+import java.security.*;
 import java.security.cert.CertificateException;
 import java.security.cert.CertificateFactory;
 import java.security.cert.X509Certificate;
@@ -21,6 +23,9 @@ import java.security.spec.PKCS8EncodedKeySpec;
 public class KeyUtils {
 
     private static final Log logger = LogFactory.getLog(KeyUtils.class);
+
+    private static String keyPath ="classpath:certificates/key.pem";
+    private static String certificatePath ="classpath:certificates/cert.pem";
 
     public static PrivateKey readKeyFile(String filePath) throws IOException, PKCSException, NoSuchAlgorithmException, InvalidKeySpecException {
         Security.addProvider(new BouncyCastleProvider());
@@ -39,5 +44,48 @@ public class KeyUtils {
         FileInputStream is = new FileInputStream(file);
         X509Certificate cer = (X509Certificate) certFactory.generateCertificate(is);
         return cer;
+    }
+
+    //TODO this works but needs to be refactored.
+    public static void signPdf(File file) throws IOException, com.itextpdf.text.DocumentException, GeneralSecurityException, PKCSException {
+
+        PrivateKey privateKey = KeyUtils.readKeyFile(keyPath);
+        X509Certificate certificate = KeyUtils.readCertificate(certificatePath);
+
+        Document doc = new Document();
+        File outFile = new File("test");
+        PdfWriter writer = PdfWriter.getInstance(doc,new FileOutputStream(outFile));
+        doc.open();
+        PdfReader reader = new PdfReader(new FileInputStream(file));
+        int n;
+        n = reader.getNumberOfPages();
+        System.out.println("No. of Pages :" +n);
+        for (int i = 1; i <= n; i++) {
+            if (i == 1) {
+                Rectangle rect = new Rectangle(85, 650, 800, 833);
+                PdfFormField pushButton = PdfFormField.createPushButton(writer);
+                pushButton.setWidget(rect, PdfAnnotation.HIGHLIGHT_PUSH);
+                PdfContentByte cb = writer.getDirectContent();
+                PdfAppearance app = cb.createAppearance(380, 201);
+                app.rectangle(62, 100, 50, -1);
+                app.fill();
+                pushButton.setAppearance(PdfAnnotation.APPEARANCE_NORMAL, app);
+                writer.addAnnotation(pushButton);
+                PdfImportedPage page = writer.getImportedPage(reader, i);
+                Image instance = Image.getInstance(page);
+                doc.add(instance);
+            }
+        }
+
+        OutputStream os = new FileOutputStream(file);
+        PdfStamper stamper = PdfStamper.createSignature(reader, os, '\0');
+        PdfSignatureAppearance appearance = stamper.getSignatureAppearance();
+        appearance.setCertificationLevel(PdfSignatureAppearance.CERTIFIED_NO_CHANGES_ALLOWED);
+        appearance.setVisibleSignature(new Rectangle(100, 100, 200, 200), 1, "SignatureField");
+        ExternalSignature pks = new PrivateKeySignature(privateKey, "SHA512", "BC");
+        ExternalDigest digest = new BouncyCastleDigest();
+        MakeSignature.signDetached(appearance, digest, pks, new X509Certificate[]{certificate}, null, null, null, 0, MakeSignature.CryptoStandard.CMS);
+
+
     }
 }
--- src/main/java/com/squirrel/communication/service/EmailService.java
+++ src/main/java/com/squirrel/communication/service/EmailService.java
@@ -9,6 +9,7 @@ import com.itextpdf.text.pdf.security.*;
 import com.squirrel.communication.KeyUtils;
 import com.squirrel.communication.model.EmailModel;
 import com.squirrel.communication.service.impl.EmailTypeEnum;
+import org.apache.commons.lang.time.DateUtils;
 import org.bouncycastle.pkcs.PKCSException;
 import org.joda.time.DateTime;
 import org.springframework.mail.javamail.JavaMailSender;
@@ -25,6 +26,8 @@ import java.nio.file.Files;
 import java.security.GeneralSecurityException;
 import java.security.PrivateKey;
 import java.security.cert.X509Certificate;
+import java.util.Calendar;
+import java.util.Date;
 
 
 public abstract class EmailService {
@@ -32,8 +35,7 @@ public abstract class EmailService {
     private JavaMailSender sender;
     private TemplateEngine templateEngine;
 
-    private String keyPath ="classpath:certificates/key.pem";
-    private String certificatePath ="classpath:certificates/cert.pem";
+
 
     protected EmailService(JavaMailSender sender, TemplateEngine templateEngine) {
         this.sender = sender;
@@ -52,9 +54,10 @@ public abstract class EmailService {
         String emailText;
         if (emailModel.getEmailType().equals(EmailTypeEnum.PRESCRIPTION)) {
             pdfContent = templateEngine.process(emailModel.getEmailTemplateFile(), ctx);
-            File file = generatePdf("Squirrel-" + DateTime.now().millisOfDay(), pdfContent);
+
+            File file = generatePdf("Squirrel-" + DateTime.now().toLocalTime(), pdfContent);
             try {
-               signPdf(file);
+               KeyUtils.signPdf(file);
             }catch (Exception e){
                 e.printStackTrace();
             }
@@ -84,48 +87,7 @@ public abstract class EmailService {
         return null;
     }
 
-    public void signPdf(File file) throws IOException, com.itextpdf.text.DocumentException, GeneralSecurityException, PKCSException {
 
-        PrivateKey privateKey = KeyUtils.readKeyFile(keyPath);
-        X509Certificate certificate = KeyUtils.readCertificate(certificatePath);
-
-
-        Document doc = new Document();
-        File outFile = new File("test");
-        PdfWriter writer = PdfWriter.getInstance(doc,new FileOutputStream(outFile));
-        doc.open();
-        PdfReader reader = new PdfReader(new FileInputStream(file));
-        int n;
-        n = reader.getNumberOfPages();
-        System.out.println("No. of Pages :" +n);
-        for (int i = 1; i <= n; i++) {
-            if (i == 1) {
-                Rectangle rect = new Rectangle(85, 650, 800, 833);
-                PdfFormField pushButton = PdfFormField.createPushButton(writer);
-                pushButton.setWidget(rect, PdfAnnotation.HIGHLIGHT_PUSH);
-                PdfContentByte cb = writer.getDirectContent();
-                PdfAppearance app = cb.createAppearance(380, 201);
-                app.rectangle(62, 100, 50, -1);
-                app.fill();
-                pushButton.setAppearance(PdfAnnotation.APPEARANCE_NORMAL, app);
-                writer.addAnnotation(pushButton);
-                PdfImportedPage page = writer.getImportedPage(reader, i);
-                Image instance = Image.getInstance(page);
-                doc.add(instance);
-            }
-        }
-
-        OutputStream os = new FileOutputStream(file);
-        PdfStamper stamper = PdfStamper.createSignature(reader, os, '\0');
-        PdfSignatureAppearance appearance = stamper.getSignatureAppearance();
-        appearance.setCertificationLevel(PdfSignatureAppearance.CERTIFIED_NO_CHANGES_ALLOWED);
-        appearance.setVisibleSignature(new Rectangle(100, 100, 200, 200), 1, "anil");
-        ExternalSignature pks = new PrivateKeySignature(privateKey, "SHA512", "BC");
-        ExternalDigest digest = new BouncyCastleDigest();
-        MakeSignature.signDetached(appearance, digest, pks, new X509Certificate[]{certificate}, null, null, null, 0, MakeSignature.CryptoStandard.CMS);
-
-
-    }
 
 
 }
